698d4bbb723be8190064520d757d375b2d950d2e
ornob011 <ornob011@gmail.com>
2023-10-31 19:03:34 +0600

    feat: refactor PaymentController to adhere to SRP

--- src/main/java/com/dsi/rjsc/payment/controller/PaymentController.java
+++ src/main/java/com/dsi/rjsc/payment/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.dsi.rjsc.payment.controller;
 
 import com.dsi.rjsc.payment.dto.PaymentDTO;
 import com.dsi.rjsc.payment.service.PaymentRequestService;
+import com.dsi.rjsc.payment.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,74 +17,16 @@ import java.util.Map;
 public class PaymentController {
 
     @Autowired
-    private PaymentRequestService paymentRequestService;
-
-    @Value("${service.url.token}")
-    private String serviceUrlToken;
-
-    @Value("${post.data.token}")
-    private String postDataToken;
-
-    @Value("${service.url.ecomm}")
-    private String serviceUrlEcomm;
-
-    @Value("${payment.merchantId}")
-    private String merchantId;
-
-    @Value("${payment.amount}")
-    private String amount;
-
-    @Value("${payment.currency}")
-    private String currency;
-
-    @Value("${payment.description}")
-    private String description;
-
-    @Value("${payment.approveUrl}")
-    private String approveUrl;
-
-    @Value("${payment.cancelUrl}")
-    private String cancelUrl;
-
-    @Value("${payment.declineUrl}")
-    private String declineUrl;
-
-    @Value("${payment.userName}")
-    private String userName;
-
-    @Value("${payment.passWord}")
-    private String passWord;
+    private PaymentService paymentService;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/")
     public String index(Model model) {
         try {
-            Map<String, Object> tokenData = paymentRequestService.processRequest(postDataToken, serviceUrlToken);
-            if (tokenData == null) {
-                logger.error(":: ERROR:: Token data is null");
-                return "redirect:/errorPage";
-            }
-
-            String transactionId = (String) tokenData.get("transactionId");
-            PaymentDTO paymentDTO = new PaymentDTO(merchantId, amount, currency, description, approveUrl, cancelUrl, declineUrl, userName, passWord, transactionId);
-            String postDataEcomm = paymentDTO.toJSON();
-
-            Map<String, Object> ecommData = paymentRequestService.processRequest(postDataEcomm, serviceUrlEcomm);
-            if (ecommData == null) {
-                logger.error(":: ERROR:: Ecomm data is null");
-                return "redirect:/errorPage";
-            }
-
-            Map<String, Object> items = (Map<String, Object>) ecommData.get("items");
-            if (items != null) {
-                String redirectUrl = items.get("url") + "?ORDERID=" + items.get("orderId") + "&SESSIONID=" + items.get("sessionId");
-                model.addAttribute("redirectUrl", redirectUrl);
-                return "redirect";
-            } else {
-                logger.error(":: ERROR:: Items map is null");
-                return "redirect:/errorPage";
-            }
+            String redirectUrl = paymentService.processPayment();
+            model.addAttribute("redirectUrl", redirectUrl);
+            return "redirect";
         } catch (Exception ex) {
             logger.error(":: ERROR:: Exception occurred :: ", ex);
             return "redirect:/errorPage";
