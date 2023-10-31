f974a09c3b0f28452060157e28012f3007183a2e
ornob011 <ornob011@gmail.com>
2023-10-31 17:52:49 +0600

    refactor: inject SSL configuration to make the service modular

--- src/main/java/com/dsi/rjsc/payment/service/PaymentRequestService.java
+++ src/main/java/com/dsi/rjsc/payment/service/PaymentRequestService.java
@@ -1,63 +1,28 @@
 package com.dsi.rjsc.payment.service;
 
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
-import org.apache.hc.client5.http.ssl.TrustSelfSignedStrategy;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
+import com.dsi.rjsc.payment.factory.RestTemplateFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import java.io.ByteArrayInputStream;
-import java.security.KeyStore;
-import java.util.Base64;
 import java.util.Map;
 
 @Service
 public class PaymentRequestService {
 
-    public Map<String, Object> processRequest(String requestBody, String serviceUrl, String pkcs12Base64, String pkcs12Password) {
+    @Value("${pkcs12.base64}")
+    private String pkcs12Base64;
+
+    @Value("${pkcs12.password}")
+    private String pkcs12Password;
+
+    @Autowired
+    private RestTemplateFactory restTemplateFactory;
+
+    public Map<String, Object> processRequest(String requestBody, String serviceUrl) {
         try {
-            // Decode the Base64 encoded PKCS12 keystore content
-            byte[] pkcs12Data = Base64.getDecoder().decode(pkcs12Base64);
-
-            // Load the PKCS12 keystore from a byte array
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            keyStore.load(new ByteArrayInputStream(pkcs12Data), pkcs12Password.toCharArray());
-
-            // Allow self-signed certificate
-            SSLContext sslContext = SSLContextBuilder
-                    .create()
-                    .loadTrustMaterial(new TrustSelfSignedStrategy())
-                    .loadKeyMaterial(keyStore, pkcs12Password.toCharArray())
-                    .build();
-
-            // Disable hostname verification
-            HostnameVerifier allHostsValid = (hostname, session) -> true;
-
-            // Build HttpClient with custom SSL context
-            SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
-                    .setSslContext(sslContext)
-                    .setHostnameVerifier(allHostsValid)
-                    .build();
-
-            CloseableHttpClient httpClient = HttpClients.custom()
-                    .setConnectionManager(
-                            PoolingHttpClientConnectionManagerBuilder.create()
-                                    .setSSLSocketFactory(sslSocketFactory)
-                                    .build()
-                    )
-                    .build();
-
-            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-
-            RestTemplate restTemplate = new RestTemplate(factory);
+            RestTemplate restTemplate = restTemplateFactory.createRestTemplate(pkcs12Base64, pkcs12Password);
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
