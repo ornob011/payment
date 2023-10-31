fce0152eaf3434beeb3e3427661dba69379dfa75
ornob011 <ornob011@gmail.com>
2023-10-31 19:06:35 +0600

    refactor: remove unused import

--- src/main/java/com/dsi/rjsc/payment/controller/PaymentController.java
+++ src/main/java/com/dsi/rjsc/payment/controller/PaymentController.java
@@ -1,17 +1,12 @@
 package com.dsi.rjsc.payment.controller;
 
-import com.dsi.rjsc.payment.dto.PaymentDTO;
-import com.dsi.rjsc.payment.service.PaymentRequestService;
 import com.dsi.rjsc.payment.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @Controller
 public class PaymentController {
